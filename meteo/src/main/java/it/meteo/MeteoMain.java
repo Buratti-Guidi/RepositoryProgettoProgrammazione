9de92b280bd03d24b6187fb425c80c29cab067b7
Christopher Buratti <s1093726@studenti.univpm.it>
2020-12-11 18:02:51 +0100

    inizio

---
+++ meteo/src/main/java/it/meteo/MeteoMain.java
@@ -0,0 +1,13 @@
+package it.meteo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeteoMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeteoMain.class, args);
+	}
+
+}
---
+++ meteo/src/test/java/it/meteo/MeteoMainTests.java
@@ -0,0 +1,13 @@
+package it.meteo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeteoMainTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
