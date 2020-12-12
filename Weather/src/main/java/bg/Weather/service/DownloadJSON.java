886e931e699f2c641904631281fe43da83821bd2
LucaGuidi5 <s1094518@studenti.univpm.it>
2020-12-12 12:42:36 +0100

    Aggiuto file con le capitali e altre modifiche
    Modificato il DownloadJSON e inizializzato JSONCapitalParser

--- Weather/src/main/java/bg/Weather/service/DownloadJSON.java
+++ Weather/src/main/java/bg/Weather/service/DownloadJSON.java
@@ -1,9 +1,12 @@
 package bg.Weather.service;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
 import java.net.URL;
 import java.net.URLConnection;
 
@@ -38,7 +41,8 @@ public class DownloadJSON {
 		this.jo = jo;
 	}
 	
-	public void chiamataAPI(String url, boolean isObject) {
+	//Legge da chiamata API un oggetto
+	public void chiamataAPIobj(String url) {
 		try {
 			URLConnection openConnection = new URL(url).openConnection();
 			InputStream in = openConnection.getInputStream();
@@ -56,15 +60,8 @@ public class DownloadJSON {
 			} finally {
 			   in.close();
 			}
-			//System.out.println("Dati scaricati: "+data);
-			if(isObject) {
+			
 				this.jo = (JSONObject) JSONValue.parseWithException(data);	 //parse JSON Object
-				System.out.println("JSONObject scaricato: "+ this.jo);
-			} else {
-				this.ja = (JSONArray) JSONValue.parseWithException(data);	//parse JSON Array
-				System.out.println("JSONArray scaricato: "+ this.ja);
-				System.out.println("IL JSONArray scaricato ha "+ this.ja.size()+" elementi:");
-			}
 				
 		} catch (IOException | ParseException e) {
 			e.printStackTrace();
@@ -72,4 +69,65 @@ public class DownloadJSON {
 			e.printStackTrace();
 		}
 	}
+	
+	//Legge da una chiamata API un array
+	public void chiamataAPIarr(String url) {
+		try {
+			URLConnection openConnection = new URL(url).openConnection();
+			InputStream in = openConnection.getInputStream();
+			
+			String data = "";
+			String line = "";
+			
+			try {
+			   InputStreamReader inR = new InputStreamReader( in );
+			   BufferedReader buf = new BufferedReader( inR );
+			  
+			   while ( ( line = buf.readLine() ) != null ) {
+				   data+= line;
+			   }
+			} finally {
+			   in.close();
+			}
+			
+			this.ja = (JSONArray) JSONValue.parseWithException(data);	//parse JSON Array
+				
+		} catch (IOException | ParseException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//Legge da file.json un oggetto
+	public void caricaFileObj(String nome_file) {
+		try {
+			ObjectInputStream file_input = new ObjectInputStream(new BufferedInputStream(new FileInputStream(nome_file)));	
+			
+			this.jo = (JSONObject) file_input.readObject();
+			
+			file_input.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//Legge da file.json un array
+	public void caricaFileArr(String nome_file) {
+		try {
+			ObjectInputStream file_input = new ObjectInputStream(new BufferedInputStream(new FileInputStream(nome_file)));
+			
+			this.ja = (JSONArray) file_input.readObject();
+				
+			file_input.close();
+				
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
---
+++ Weather/src/main/java/bg/Weather/service/JSONCapitalParser.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package bg.Weather.service;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+/**
+ * @author Luca
+ *
+ */
+public class JSONCapitalParser {
+
+	
+	public void parseJSON(JSONArray arr) {
+		
+		
+
+	}
+
+}
