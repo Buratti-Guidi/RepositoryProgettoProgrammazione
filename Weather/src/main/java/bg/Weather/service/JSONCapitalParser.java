257fdf9c324ba64bca0c5fd0e2501fa9f4da846a
LucaGuidi5 <s1094518@studenti.univpm.it>
2020-12-12 13:07:10 +0100

    Aggiornato CapitalParser

--- Weather/src/main/java/bg/Weather/service/JSONCapitalParser.java
+++ Weather/src/main/java/bg/Weather/service/JSONCapitalParser.java
@@ -3,6 +3,8 @@
  */
 package bg.Weather.service;
 
+import java.util.HashSet;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
@@ -12,10 +14,24 @@ import org.json.simple.JSONObject;
  */
 public class JSONCapitalParser {
 
+	private HashSet<String> capitals = new HashSet<String>();//vettore di capitali
 	
-	public void parseJSON(JSONArray arr) {
+	
+	public void parseJSON(JSONArray ja) {
 		
+		try {	
 		
+			for(Object o : ja) {
+				
+				JSONObject citta = (JSONObject) o;
+				String app = (String)citta.get("capital");
+				
+				capitals.add(app);
+				
+			}
+		}catch(Exception e) {//non so che tipo di eccezione puo generare
+			System.out.println("Errore sul parsing");
+		}
 
 	}
 
